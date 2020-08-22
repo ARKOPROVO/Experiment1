eb182c220da02a465768892f43d704dcd32c0757
ctsdotnetuser111 <ctsdotnetuser111@CTSDOTNET26>
2020-08-22 16:15:42 +0530

    Minor change

--- WebApplication2/Controllers/WeatherForecastController.cs
+++ WebApplication2/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace WebApplication2.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "ScorchingHot"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
